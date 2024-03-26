c3289676e1e6494b664e36e68816f50ede41e793
NickTrinh <nhattvm1112@gmail.com>
2024-03-26 19:07:48 -0400

    Optimized CPU usage.

--- main.py
+++ main.py
@@ -8,10 +8,25 @@ def send_notification(title, message):
         message = message
     )
 
-def main():
+def run_reminders():
+    while True:
+        next_run = schedule.idle_seconds()
+        if next_run is None:
+            # No more jobs; can break or continue as needed
+            break
+        elif next_run > 0:
+            # Sleep until the next scheduled job should run
+            time.sleep(next_run)
+        schedule.run_pending()
+
+
+def setup_reminders():
     schedule.every(1).hours.do(send_notification, title="Hydration Time!", message="Take a moment to drink some water.")
     schedule.every(2).hours.do(send_notification, title="Break Time!", message="Time to stretch and take a short break.")
-    time.sleep(1)
+
+def main():
+    setup_reminders()
+    run_reminders()
 
 
 if __name__ == "__main__":
